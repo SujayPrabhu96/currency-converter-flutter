20d567e7b8832a46979ca3bd92dac2b29da653aa
SujayPrabhu96 <prabhusujay@gmail.com>
2023-11-18 11:58:15 +0530

    Add TextField

--- lib/currency_converter_material_page.dart
+++ lib/currency_converter_material_page.dart
@@ -6,17 +6,36 @@ class CurrencyConverterMaterialPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
+      backgroundColor: Colors.black,
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             Text(
-              'Hello World!!!',
+              '100',
               style: TextStyle(
                 fontSize: 55,
-                fontWeight: FontWeight.bold
+                fontWeight: FontWeight.bold,
+                color: Colors.white
               )
+            ),
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Please enter the amount in INR',
+                labelStyle: TextStyle(
+                  color: Colors.white
+                ),
+                prefixIcon: Icon(IconData(0xf05db, fontFamily: 'MaterialIcons')),
+                border: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.white
+                  ),
+                )
+              ),
+              style: TextStyle(
+                color: Colors.white
+              ),
             )
           ],
         )  
